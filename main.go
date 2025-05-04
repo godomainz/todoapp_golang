b86bc9505a3f7b2a51ee66c49d5c8859a18c6784
Akila Mendis <godomainz@gmail.com>
2025-05-04 15:49:23 +0530

    Variables in Go

--- main.go
+++ main.go
@@ -3,8 +3,29 @@ package main
 import "fmt"
 
 func main() {
+
+	var shortGoLang = "Watch go crash course";
+	var fullGoLang = "Watch Nana's Golang Full Course";
+	var rewardDEssert = "Reward myself with a cheesecake"
+
 	fmt.Println("###### Welcome to our Todolist App! ######")
-	fmt.Println("1. Watch go crash course")
-	fmt.Println("2. Watch Nana's Golang Full course")
-	fmt.Println("3. Reward myself with a cheesecake")
+
+	fmt.Println("List of my Todos")
+	fmt.Println(shortGoLang)
+	fmt.Println(fullGoLang)
+	fmt.Println(rewardDEssert)
+
+	fmt.Println()
+	fmt.Println("Tutorials")
+	fmt.Println(shortGoLang)
+	fmt.Println(fullGoLang)
+
+	fmt.Println()
+	fmt.Println("Rewards")
+	fmt.Println(rewardDEssert)
+
+	fmt.Println()
+	fmt.Println("My Project")
+	fmt.Println(fullGoLang)
+
 }
