38680b3ea2d2a4167a363f3ddcb46c626e39871e
Akila Mendis <godomainz@gmail.com>
2025-05-04 15:35:35 +0530

    Data Types in Go

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello world")
+	fmt.Println("###### Welcome to our Todolist App! ######")
+	fmt.Println("1. Watch go crash course")
+	fmt.Println("2. Watch Nana's Golang Full course")
+	fmt.Println("3. Reward myself with a cheesecake")
 }
