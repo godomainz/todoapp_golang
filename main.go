5cd555113489db61ab763d61ff378ccd643affe9
Akila Mendis <godomainz@gmail.com>
2025-05-04 17:44:58 +0530

    returning value from functions

--- main.go
+++ main.go
@@ -7,11 +7,15 @@ func main() {
 	shortGoLang := "Watch go crash course";
 	fullGoLang := "Watch Nana's Golang Full Course";
 	rewardDEssert := "Reward myself with a cheesecake"
-
 	taskItems := []string {shortGoLang, fullGoLang, rewardDEssert}
 
 	fmt.Println("###### Welcome to our Todolist App! ######")
 
+	printTasks(taskItems)
+
+	taskItems = addTasks(taskItems, "Go for a run")
+	
+	fmt.Println()
 	printTasks(taskItems)
 
 }
@@ -21,4 +25,9 @@ func printTasks(taskItems []string){
 	for index, task:=range(taskItems) {
 		fmt.Printf("%d. %s\n", index+1, task)
 	}
+}
+
+func addTasks(taskItems []string, newtask string) []string {
+	updateTaskItems := append(taskItems, newtask)
+	return updateTaskItems
 }
